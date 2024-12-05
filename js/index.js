d2f2f061167e161ba6c257454f3a977a5e3ddeb1
Isc740 <isaazcantillo@gmail.com>
2024-12-04 22:19:54 -0500

    add search bar

--- js/index.js
+++ js/index.js
@@ -6,12 +6,33 @@ async function loadProducts() {
             (productContainer.innerHTML += Utils.getProductCard(product)),
     );
     Utils.listenCardClick(document.querySelectorAll(".single-product-trigger"));
+    searchProduct(mainProducts);
+}
+
+function searchProduct(products) {
+    const searchInput = document.querySelector(".search-input");
+    const productContainer = document.querySelector(".product-container");
+    searchInput.addEventListener("input", (e) => {
+        if (searchInput.value === "") {
+            loadProducts();
+            return;
+        }
+
+        productContainer.innerHTML = "";
+        for (const product of products) {
+            console.log(product);
+            console.log(searchInput.value);
+            if (product.title.toLowerCase().includes(searchInput.value)) {
+                productContainer.innerHTML += Utils.getProductCard(product);
+            }
+        }
+    });
 }
 
 if (!localStorage.getItem("usersCarts")) {
     let usersCarts = [];
     for (let i = 0; i < 20; i++) {
-        usersCarts.push({ "userId": i, "products": [] })
+        usersCarts.push({ userId: i, products: [] });
     }
     localStorage.setItem("usersCarts", JSON.stringify(usersCarts));
 }
