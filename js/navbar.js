568a76121e29abc92eb4a254f3ceea2ca3f349f9
Isc740 <isaazcantillo@gmail.com>
2024-12-04 21:27:06 -0500

    add cart icon

--- js/navbar.js
+++ js/navbar.js
@@ -1,33 +1,35 @@
-const getNavbar = () => `
-	<nav
-		class="navbar navbar-expand-md bg-body-tertiary mb-5 shadow-sm navbar-light"
-		>
-		<div class="container-fluid">
-			<a class="navbar-brand" href="/index.html">Online e-comerce</a>
-			<button
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbar-toggler"
-			aria-controls="navbarSupportedContent"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-			>
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbar-toggler">
-				<ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex gap-2">
+const getNavbar = () => {
+    const userCart = Utils.getUserCart(localStorage.getItem("userId"))
+    return `
+    <nav class="navbar navbar-expand-md bg-body-tertiary mb-5 shadow-sm navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.html">Online e-comerce</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbar-toggler"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar-toggler">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex gap-2">
                     <li class="nav-item">
-                        <a class="btn btn-success" href="/html/cart.html">Cart</a>
+                        <a class="btn btn-outline-success d-flex align-items-center gap-2" href="/html/cart.html">
+                            <i class="bi bi-cart"></i>
+                            <span class="badge bg-danger">${userCart.products.length}</span>
+                        </a>
                     </li>
                     <li class="nav-item">
                         <a class="btn btn-success" href="/html/login.html">Login</a>
                     </li>
-				</ul>
-			</div>
-		</div>
-		</nav>
-	`;
+                </ul>
+            </div>
+        </div>
+    </nav>
+`};
 
 document.querySelector(".navbar-container").innerHTML = getNavbar();
 
